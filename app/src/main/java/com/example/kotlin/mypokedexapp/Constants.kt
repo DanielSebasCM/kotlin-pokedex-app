324a89371f54f81063b72523c3421970a4b10eb6
DanielSebasCM <danielsebascm@hotmail.com>
2023-08-28 12:21:40 -0600

    V1

---
+++ app/src/main/java/com/example/kotlin/mypokedexapp/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.kotlin.mypokedexapp
+
+object Constants {
+    const val MAX_POKEMON_NUMBER = 1279
+    const val BASE_URL = "https://pokeapi.co/api/v2/"
+}
---
+++ app/src/main/java/com/example/kotlin/mypokedexapp/pokemon/GenerationI.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin.mypokedexapp.pokemon
+
+// Google Library
+import com.google.gson.annotations.SerializedName
+
+data class GenerationI(
+    @SerializedName("red-blue") val red_blue: RedBlue,
+    val yellow: Yellow
+)
